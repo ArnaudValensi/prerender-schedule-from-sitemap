63cb483f97301f07e39c9c21d535fa5fd81ac10a
Arnaud Valensi <arnaud.valensi@gmail.com>
2016-09-05 18:16:46 +0200

    add a mongodb cache

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ var server = prerender({
     softIterations: 30
 });
 
+const sitemap = process.env.SITEMAP;
 
+// server.use(require('prerender-mongo'));
+// server.use(require('prerender-mongodb-cache'));
+server.use(prerender.mongoCache());
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
@@ -17,4 +21,7 @@ server.use(prerender.httpHeaders());
 // server.use(prerender.inMemoryHtmlCache());
 // server.use(prerender.s3HtmlCache());
 
-server.start();
+
+server.start(function () {
+  console.log('[+] started');
+});
