9f1db785f5c89a8af1ab2c142109880b9a6d361a
Arnaud Valensi <arnaud.valensi@gmail.com>
2016-09-06 11:57:48 +0200

    extract urls from sitemap

--- src/autorechache/index.js
+++ src/autorechache/index.js
@@ -1,10 +1,43 @@
 import config from 'config';
+import fetch from 'node-fetch';
+import { parseString } from 'xml2js';
+import _ from 'underscore';
 
-function sitemapToPages(sitemapUrl) {
-  // TODO
+function getSitemap(sitemapUrl) {
+  return fetch(sitemapUrl)
+    .then((res) => res.text());
 }
 
-function cacheAllPages(pages) {
+function xmlTojs(xmlSitemap) {
+  return new Promise((resolve, reject) => {
+    parseString(xmlSitemap, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
+}
+
+function jsSitemapToUrls(jsSitemap) {
+  return new Promise((resolve, reject) => {
+    const { urlset } = jsSitemap;
+    const { url } = urlset;
+
+    if (!urlset) {
+      return reject(new Error('Invalid sitemap'));
+    }
+
+    if (!url) {
+      console.warn('[!] no url in sitemap');
+      return resolve();
+    }
+
+    resolve(_.pluck(url, 'loc').reduce((memo, url) => memo.concat(url), []));
+  });
+}
+
+function cacheAllPages(urls) {
   // TODO
 }
 
@@ -13,14 +46,14 @@ function runCron(ttl) {
 }
 
 export default () => {
-
   const sitemapUrls = config.get('sitemaps');
 
-  console.log(sitemapUrls);
-
-  // const pages = sitemapToPages(sitemapUrl);
-  //
-  // cacheAllPages(pages, function () {
-  //   runCron(ttl);
-  // });
+  getSitemap(sitemapUrls)
+    .then(xmlTojs)
+    .then(jsSitemapToUrls)
+    // .then(cacheAllPages)
+    .then((result) => {
+      console.log(result);
+      console.log(result.length);
+    });
 };
